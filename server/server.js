7712a9fac17ee3ade399e8e5aa28edc239a0917d
freshcoderchan <freshcoderjack@outlook.com>
2018-10-04 16:52:54 -0700

    Remodule node server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const config = require('./config/config.js');
 
 // *** main dependencies *** //
 const express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -12,39 +11,108 @@ var mongoose = require('mongoose');
 var chalk = require('chalk');
 
 
-// *** config middleware *** //
-app.use(bodyParser.json());
-app.use(cookieParser()); 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false
-    }
-}));
 
-// *** main routes *** //
-require('./route/place.route')(app);
+/**
+ * config Middleware
+ */
+module.exports.initMiddleware = function (app) {
+    app.use(bodyParser.json());
+    app.use(cookieParser()); 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false
+        }
+    }));
+}
+  
+/**
+ * Set main routes
+ */
+module.exports.initModulesServerRoutes = function (app) {
+    require('./route/place.route')(app);
+};
 
-// *** connect mongoose *** //
-mongoose.connect(config.DB.uri, config.DB.options, (err) => {
-    if(err) {
-      console.log(err);
-    }
-    console.log(chalk.green('Connected MongoDB successfully'));
-});
 
-// *** create server *** //
-var server = app.listen(8080, function () {
+/**
+ * Load models
+ */
+module.exports.loadModel = function () {
+    require('./model/place.model');
+};
+
+/**
+ * Seed
+ */
+module.exports.seed = function () {
  
-    let host = server.address().address
-    let port = server.address().port
+    let places = [
+      {
+        locationName: "Place1",
+        description: "Place1",
+        latitude: '36.7',
+        longitude: '45.8'
+      },
+      {
+        locationName: "Place2",
+        description: "Place2",
+        latitude: '83.1',
+        longitude: '11.2'
+      },
+    ]
    
-    console.log("App listening at http://%s:%s", host, port);
-});
-   
+    // Init data -> save to MongoDB
+    const Place = require('./model/place.model');
+    
+    Place.create(places);  
+  }
+
+/**
+ * Create mongoose
+ */
+module.exports.connectMongoDB = function () {
+
+    mongoose.connect(config.DB.uri, config.DB.options, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(chalk.green('Connected MongoDB successfully'));
+        
+            // *** insert seed data **/
+            this.seed();
+        }
+    });
+
+}
+/**
+ * Create Http Server
+ */
+module.exports.createServer = function (app) {
+      
+    var server = app.listen(8080, function () {
+        
+        let host = server.address().address || config.Server.Host
+        let port = server.address().port
+    
+        console.log("App listening at http://%s:%s", host, port);
+    });
+
+}
+
+
+module.exports.init = function () {
+    var app = express();
+    this.loadModel();
+    this.initMiddleware(app);
+    this.initModulesServerRoutes(app);
+    this.connectMongoDB();
+    this.createServer(app);
+}
+
+this.init();
