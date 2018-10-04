3828f5a3760800a30c0ac6636dc7c292b5eb05b2
freshcoderchan <freshcoderjack@outlook.com>
2018-10-04 15:24:27 -0700

    Add functions in PlaceAddComponent, PlaceEditComponent

--- mainapp/src/app/place-add/place-add.component.ts
+++ mainapp/src/app/place-add/place-add.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../place';
+import { PlaceService } from '../place.service';
+
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-place-add',
   templateUrl: './place-add.component.html',
   styleUrls: ['./place-add.component.css']
 })
-export class PlaceAddComponent implements OnInit {
+export class PlaceAddComponent {
 
-  constructor() { }
+  place = new Place();
+  submitted = false;
 
-  ngOnInit() {
+  constructor(
+    private placeService: PlaceService,
+    private location: Location
+  ) { }
+
+  newPlace(): void {
+    this.submitted = false;
+    this.place = new Place();
+  }
+
+ addPlace() {
+   this.submitted = true;
+   this.save();
+ }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private save(): void {
+    console.log(this.place);
+    this.placeService.addPlace(this.place)
+        .subscribe();
   }
 
 }
--- mainapp/src/app/place-edit/place-edit.component.ts
+++ mainapp/src/app/place-edit/place-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../place';
+import { PlaceService } from '../place.service';
+
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-place-edit',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-edit.component.css']
 })
 export class PlaceEditComponent implements OnInit {
+  place = new Place() ;
+  submitted = false;
+  message: string;
 
-  constructor() { }
+  constructor(
+    private placeService: PlaceService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.placeService.getPlace(id)
+      .subscribe(place => this.place = place);
+  }
+
+  update(): void {
+    this.submitted = true;
+    this.placeService.updatePlace(this.place)
+        .subscribe(result => this.message = "Place Updated Successfully!");
+  }
+
+  delete(): void {
+    this.submitted = true;
+    this.placeService.deletePlace(this.place.id)
+        .subscribe(result => this.message = "Place Deleted Successfully!");
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
