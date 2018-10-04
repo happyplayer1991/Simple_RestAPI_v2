3e2ac4d3b4bf3c9287485f0f694f0303e7d887a1
freshcoderchan <freshcoderjack@outlook.com>
2018-10-04 14:43:55 -0700

    Added routes in mainapp

---
+++ mainapp/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+  // declarations: []
+})
+export class AppRoutingModule { }
