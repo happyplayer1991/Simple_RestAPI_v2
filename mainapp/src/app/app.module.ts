a18626d5666a94a7bbf2290d9f33fbdd60f4445f
freshcoderchan <freshcoderjack@outlook.com>
2018-10-04 15:28:16 -0700

    Added FormsModule and HttpClientModule in app.module

--- mainapp/src/app/app.module.ts
+++ mainapp/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { PlaceAddComponent } from './place-add/place-add.component';
 import { PlaceEditComponent } from './place-edit/place-edit.component';
 import { PlaceComponent } from './place/place.component';
@@ -16,7 +21,9 @@ import { PlaceComponent } from './place/place.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
